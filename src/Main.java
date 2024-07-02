19f2187ee9086056c0e1814bce20ea3d35aee0d1
Rajat Pratap <chauhanrajat515@gmail.com>
2024-07-02 19:55:01 +0530

    added

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter the first number : " );
+       Scanner sc = new Scanner(System.in);
+       String i = sc.nextLine();
+       int x = Integer.parseInt(i);
+        System.out.println("Enter the second number : " );
+        Scanner sce = new Scanner(System.in);
+        String j = sc.nextLine();
+        int y = Integer.parseInt(j);
+        System.out.println("Enter the operation to be performed: " );
+        Scanner ch = new Scanner(System.in);
+        String chr = ch.nextLine();
+        char f = Character.parseCha
+          int u = switch (chr){
+            case "add"-> x+y;
+            case "Subtarct" -> x-y;
+            case "Multiply" -> x*y;
+            case "Remainder " -> x%y;
+            case "divide" -> x/y;
+            default -> 0;
+        };
+          System.out.println(u);
+    }
+}
